14f79970caec0451fce485f25d45cada66fd02f0
‘evanmascitti’ <evanmascitti@gmail.com>
2021-01-23 15:22:13 -0500

    re-built beaker tares data documentation and updated readme

--- R/psa_beaker_tares.R
+++ R/psa_beaker_tares.R
@@ -1,12 +1,8 @@
 #' @title Tared beaker masses
 #'
-#' @description These beakers are used repeatedly for particle size analysis and
-#'   other lab work. Storing them in a list permits easy left joins and allows
-#'   for addition of future expansion to a given set, or for new sets of beakers
-#'   to be added.
+#' @description These beakers are used repeatedly for particle size analysis and other lab work. Storing them in a list permits easy left joins and allows for addition of future expansion or new sets of beakers to be added.
 #'
-#' @format a list of tibbles, with each containing two columns (the beaker
-#'   number and the tared mass in grams, to 0.001 g)
+#' @format a list of tibbles, with each containing two columns (the beaker number and the tared mass in grams, to 1 mg)
 #'
 #'
 #'
